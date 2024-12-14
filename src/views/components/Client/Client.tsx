72defc70d4e9e69900cae8d7e68d2e31c4db41f2
matheus-calixto-silva <matheuscalixto8@gmail.com>
2024-12-14 19:35:03 -0300

    feat: criação da pagina Inicial de Cliente

---
+++ src/views/components/Client/Client.tsx
@@ -0,0 +1,11 @@
+import AvaliableServices from '../../pages/AvaliableServices/AvaliableServices';
+
+const Client = () => {
+  return (
+    <div>
+      <AvaliableServices />
+    </div>
+  );
+};
+
+export default Client;
